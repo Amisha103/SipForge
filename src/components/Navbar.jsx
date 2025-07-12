a844d2e507da006444321572a64b2389a0b3971c
amisha103 <bhatiaamisha74@gmail.com>
2025-07-12 17:06:54 +0530

    .env fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,12 @@ import logo from '../assets/logo.jpg';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <img src={logo} alt="SipForge Logo" className="logo" />
-        <span className="brand-name">SipForge</span>
-      </div>
+     <div className="navbar-left">
+  <Link to="/">
+    <img src={logo} alt="SipForge Logo" className="logo" />
+  </Link>
+  <span className="brand-name">SipForge</span>
+</div>
       <ul className="navbar-links">
            <li><Link to="/">Home</Link></li>
         <li><Link to="/services">Services</Link></li>
